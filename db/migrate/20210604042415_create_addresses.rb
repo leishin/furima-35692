b552416e22e15a7cd3dc927a4c0f4788858e0dc4
leishin <83324799+leishin@users.noreply.github.com>
2021-06-09 17:34:06 +0900

    修正3

--- db/migrate/20210604042415_create_addresses.rb
+++ db/migrate/20210604042415_create_addresses.rb
@@ -3,9 +3,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string     :postcode,   null: false
       t.integer    :prefecture_id,    null: false
-      t.string     :city
-      t.string     :block
-      t.string     :phone_number
+      t.string     :city,  null: false
+      t.string     :block,  null: false
+      t.string     :phone_number,  null: false
       t.string     :building
       t.references :purchase_record,      null: false, foreign_key: true
       t.timestamps
